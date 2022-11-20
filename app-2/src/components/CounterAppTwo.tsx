ef3e916e4a8a1559c0755ce8308e4af38194f651
Arnald Dutta <arnaldmib@gmail.com>
2022-11-20 12:51:04 +0530

    Added react router feature in mf Moved container app to component Added .env file Added babel config

--- app-2/src/components/CounterAppTwo.tsx
+++ app-2/src/components/CounterAppTwo.tsx
@@ -1,16 +1,26 @@
-import { Button, Flex, Text } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Button, Flex, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Counter = () => {
   const [count, setCount] = useState(1);
 
+  const location = useLocation();
+
   return (
-    <Flex gap="1rem" direction="column">
+    <Flex color="#000" gap="1rem" direction="column">
       <Text>
         Add by one each click <strong>APP-2</strong>
       </Text>
       <Text>Your click count : {count} </Text>
-      <Button onClick={() => setCount((prevState) => prevState * 2)}>Click me</Button>
+      <Button onClick={() => setCount((prevState) => prevState * 2)}>
+        Click me
+      </Button>
+      {location.pathname !== "/" && (
+        <Button as={Link} to="/">
+          Back to container
+        </Button>
+      )}
     </Flex>
   );
 };
--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,29 +1,54 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack'); // only add this if you don't have yet
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack"); // only add this if you don't have yet
 const { ModuleFederationPlugin } = webpack.container;
-const deps = require('./package.json').dependencies;
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const deps = require("./package.json").dependencies;
+require("dotenv").config({ path: "./.env" });
 
 const buildDate = new Date().toLocaleString();
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-  console.log({ isProduction });
+  const isProduction = argv.mode === "production";
   return {
-    entry: './src/index.ts',
-    mode: process.env.NODE_ENV || 'development',
+    entry: "./src/index.ts",
+    mode: process.env.NODE_ENV || "development",
     devServer: {
       port: 3000,
       open: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
     },
     resolve: {
-      extensions: ['.ts', '.tsx', '.js'],
+      extensions: [".ts", ".tsx", ".js"],
     },
     module: {
       rules: [
         {
           test: /\.(js|jsx|tsx|ts)$/,
-          loader: 'ts-loader',
+          loader: "babel-loader",
           exclude: /node_modules/,
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            presets: [
+              [
+                "@babel/preset-env",
+                { targets: { browsers: "last 2 versions" } },
+              ],
+              "@babel/preset-typescript",
+              "@babel/preset-react",
+            ],
+//            plugins: [
+//              "react-hot-loader/babel",
+//              ["@babel/plugin-proposal-class-properties", { loose: true }],
+//              [
+//                "@babel/plugin-proposal-private-property-in-object",
+//                { loose: true },
+//              ],
+//              ["@babel/plugin-proposal-private-methods", { loose: true }],
+//            ],
+          },
         },
       ],
     },
@@ -31,28 +56,33 @@ module.exports = (env, argv) => {
     plugins: [
       new webpack.EnvironmentPlugin({ BUILD_DATE: buildDate }),
       new webpack.DefinePlugin({
-        'process.env': JSON.stringify(process.env),
+        "process.env": JSON.stringify(process.env),
       }),
       new ModuleFederationPlugin({
-        name: 'container',
+        name: "container",
         remotes: {
-          app1: 'app1@http://localhost:3001/remoteEntry.js',
-          app2: 'app2@http://localhost:3002/remoteEntry.js',
+          app1: isProduction ? process.env.PROD_APP1 : process.env.DEV_APP1,
+          app2: isProduction ? process.env.PROD_APP2 : process.env.DEV_APP2,
         },
-        exposes: {},
         shared: {
           ...deps,
           react: { singleton: true, eager: true, requiredVersion: deps.react },
-          'react-dom': {
+          "react-dom": {
             singleton: true,
             eager: true,
-            requiredVersion: deps['react-dom'],
+            requiredVersion: deps["react-dom"],
+          },
+          "react-router-dom": {
+            singleton: true,
+            eager: true,
+            requiredVersion: deps["react-router-dom"],
           },
         },
       }),
       new HtmlWebpackPlugin({
-        template: './public/index.html',
+        template: "./public/index.html",
       }),
+      new ForkTsCheckerWebpackPlugin(),
     ],
   };
 };
